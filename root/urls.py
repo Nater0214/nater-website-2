5f96cc6c71aaa7ae4219dcc83a052bd78f28b862
Nater0214 <gamer.nater0214@gmail.com>
2023-07-06 23:59:54 -0400

    Add tools

--- root/urls.py
+++ root/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about-me", views.about_me, name="about_me"),
     path("about-site", views.about_site, name="about_site"),
+    path("tools", views.tools, name="tools"),
+    path("tools/popup-maker", views.popup_maker, name="popup_maker"),
 ]
