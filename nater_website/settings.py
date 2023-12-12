eef174d4a47fdef3eab80d32eb352a9480c15294
Nater0214 <gamer.nater0214@gmail.com>
2023-12-12 18:25:00 -0500

    Fix static url

--- nater_website/settings.py
+++ nater_website/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
